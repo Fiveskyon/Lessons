92d889c363ca9093b8a575229c0c7929e67a489b
Alexander <89515014507@mail.ru>
2023-01-24 22:25:28 +0600

    Урок 8 закончен

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,37 @@
-
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-//        House dom = new House ();
-//        dom.doore = 2;
+
+
+        int[] nums = {13,16,0,436,89,9}; // {250}
+//
+//        NumbersArray arraus = new NumbersArray ();
+//        arraus.setNumsArray ( nums );
+//        arraus.add ( 675 );
+//        arraus.printArrays ();
+//        arraus.setNumsArray ( new int[]{13,16,0,436,89,9} );
+
+
+        // =============================== Static
+        ArrayNums.printArray ( nums );
+        nums = ArrayNums.setLengthMore ( nums, 250 );
+        ArrayNums.printArray ( nums );
+
 
     }
+
+    /**
+     *  Задание №_4
+     *  Ответь на вопрос что такое ООП и для чего он нужен
+     *
+     *  Задание №_5
+     *  Рассказать как ты понимаешь что такое модификаторы доступа
+     *
+     *  Задание №_6
+     *  Рассказать про структуру класса
+     */
 
 
 

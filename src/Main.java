f5d935c053aab8aad2acd6104bc0726619102d49
Alexander <89515014507@mail.ru>
2023-01-24 21:43:17 +0600

    sdfsdfhsd

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        Cat cat1 = new Cat ();
-        cat1.setName ( "Мурзик" );
-        Cat cat2 = new Cat ();
-        cat2.setName ( "Барсик" );
-
-
-//        System.out.println (cat2.getName () );
-        cat2.printCatKatenok ();
+//        House dom = new House ();
+//        dom.doore = 2;
 
     }
 
